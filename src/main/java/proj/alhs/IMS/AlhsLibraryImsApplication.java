edc69f4aac508a17fd7b11931b4ccd93cb62ec6b
lithobraking <34843669+lithobraking@users.noreply.github.com>
2021-09-27 17:07:58 -0700

    Initial commit

---
+++ src/main/java/proj/alhs/IMS/AlhsLibraryImsApplication.java
@@ -0,0 +1,13 @@
+package proj.alhs.IMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlhsLibraryImsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlhsLibraryImsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proj/alhs/IMS/AlhsLibraryImsApplicationTests.java
@@ -0,0 +1,13 @@
+package proj.alhs.IMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlhsLibraryImsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
