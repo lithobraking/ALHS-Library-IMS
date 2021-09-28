a3a5577f56ef532fd35489886fe7df7f1bbb05df
lithobraking <34843669+lithobraking@users.noreply.github.com>
2021-09-28 10:30:40 -0700

    mated students API to postgres database

--- src/main/java/proj/alhs/IMS/students/Student.java
+++ src/main/java/proj/alhs/IMS/students/Student.java
@@ -1,10 +1,24 @@
 package proj.alhs.IMS.students;
+import javax.persistence.*;
 
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String firstName;
     private String lastName;
     private Integer itemsReserved;
+
 
     public Student() {
     }
