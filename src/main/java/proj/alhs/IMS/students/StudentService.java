bc492f27751f405f720193b83dc5368fabe4062d
lithobraking <34843669+lithobraking@users.noreply.github.com>
2021-09-29 11:57:10 -0700

    StudentController post mapping adds new student to db

--- src/main/java/proj/alhs/IMS/students/StudentService.java
+++ src/main/java/proj/alhs/IMS/students/StudentService.java
@@ -2,6 +2,7 @@ package proj.alhs.IMS.students;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -19,6 +20,11 @@ public class StudentService {
     }
 
     public void addStudent(Student student) {
-        System.out.println(student);
+        Optional<Student> existingStudent = studentRepository.findStudentByEmail(student.getEmail());
+        if (existingStudent.isPresent()) {
+            throw new IllegalStateException("Sorry, but this email is already in use.");
+        } else {
+            studentRepository.save(student);
+        }
     }
 }
